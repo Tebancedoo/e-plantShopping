498687908166883d57fe72439c8bd4f133c543ed
esteban salcedoe <estebansalcedo211@gmail.com>
2024-10-27 13:52:43 -0400

    changed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,22 +1,39 @@
+// importamos
 import { createSlice } from '@reduxjs/toolkit';
 
+//exportamos
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [], // inicializa los item
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
+
+// Selector para la cantidad total de artículos en el carrito
+export const selectTotalQuantity = (state) => {
+  return state.cart.items.reduce((total, item) => total + item.quantity, 0);
+};
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
+//importamos
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+
+
+//variable principal
  const store = configureStore({
     reducer: {
         cart: cartReducer,
     },
 });
+
+//exportamos
 export default store
